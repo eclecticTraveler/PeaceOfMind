f2b0987a679af34d187637f226e53a6d8faf0872
Alberto Arellano <arellana@byui.edu>
2020-09-01 06:53:36 -0600

    adding interface to repo layer

---
+++ PeaceOfMind.BusinessLogic/Repositories/ISurveyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using PeaceOfMind.Service.Models;
+
+namespace PeaceOfMind.Service.Repositories
+{
+    public interface ISurveyRepository
+    {
+        IEnumerable<ISurvey> GetAvailableDistinctSurveys();
+        IEnumerable<IQuestion> GetSurveyQuestions(int surveyId, IEnumerable<IQuestion> questions);
+        void SaveSurveyResult(ISurveyResult surveyResult);
+        IEnumerable<ISurveyResult> GetSurveyResultsPerPeriod(DateTime minDate, DateTime maxDate);
+    }
+}
